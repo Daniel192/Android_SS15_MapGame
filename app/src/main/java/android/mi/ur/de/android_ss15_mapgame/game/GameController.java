7b99c8b5fa8a5eba4965d73901f369f64ded4e62
Daniel192 <d.hecht13@gmx.de>
2015-08-24 21:47:07 +0200

    Added: #1 Project setup
    Created most classes and activities needed for the project.

---
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/game/GameController.java
@@ -0,0 +1,7 @@
+package android.mi.ur.de.android_ss15_mapgame.game;
+
+/**
+ * Created by Daniel on 24.08.2015.
+ */
+public class GameController {
+}
---
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/persistence/LocalHighscoreDb.java
@@ -0,0 +1,7 @@
+package android.mi.ur.de.android_ss15_mapgame.persistence;
+
+/**
+ * Created by Daniel on 24.08.2015.
+ */
+public class LocalHighscoreDb {
+}
