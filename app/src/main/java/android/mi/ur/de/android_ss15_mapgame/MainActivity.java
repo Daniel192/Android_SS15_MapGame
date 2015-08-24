8d6181acf1daee59aacf8276a43feaffa8f1b39d
Oliver Pöppel <oliver93@hotmail.de>
2015-08-24 16:35:36 +0200

    Fixed: #1 Project Setup with Google Maps and Key

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/MainActivity.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/MainActivity.java
@@ -1,17 +1,21 @@
 package android.mi.ur.de.android_ss15_mapgame;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent myIntent = new Intent(MainActivity.this, MapActivity.class);
+        startActivity(myIntent);
     }
 
 
---
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/MapActivity.java
@@ -0,0 +1,35 @@
+package android.mi.ur.de.android_ss15_mapgame;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * This shows how to create a simple activity with a map and a marker on the map.
+ */
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+}
