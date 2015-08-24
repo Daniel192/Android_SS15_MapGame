e9cd1386f89d0425506e64ed5f8ee55aff8c381a
Daniel192 <d.hecht13@gmx.de>
2015-08-24 21:54:33 +0200

    TestCommit

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameActivity.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameActivity.java
@@ -1,7 +1,19 @@
 package android.mi.ur.de.android_ss15_mapgame.activities;
 
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MapActivity;
+import android.mi.ur.de.android_ss15_mapgame.R;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Daniel on 24.08.2015.
  */
-public class GameActivity {
+public class GameActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
