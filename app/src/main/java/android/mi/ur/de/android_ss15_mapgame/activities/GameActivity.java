6a42db42b59b056b75e7a15c95e37b9931a3b16b
raffaela007 <raffaela.schierle@stud.uni-regensburg.com>
2015-08-30 20:21:14 +0200

    #3 Created Layout

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameActivity.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameActivity.java
@@ -4,16 +4,35 @@ import android.content.Intent;
 import android.mi.ur.de.android_ss15_mapgame.MapActivity;
 import android.mi.ur.de.android_ss15_mapgame.R;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Daniel on 24.08.2015.
- */
-public class GameActivity extends AppCompatActivity{
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class GameActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.game_activity);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.gameMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+
     }
 }
+
