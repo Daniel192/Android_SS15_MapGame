5a3836ba298c20aebd33ab856fbeda1ba8f915e0
Daniel192 <d.hecht13@gmx.de>
2015-09-04 15:36:37 +0200

    Added: #12 Created QuestionItem

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/utility/QuestionItem.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/utility/QuestionItem.java
@@ -1,7 +1,26 @@
 package android.mi.ur.de.android_ss15_mapgame.utility;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by Daniel on 31.08.2015.
  */
 public class QuestionItem {
+
+    private String question;
+    private LatLng targetLocation;
+
+    public QuestionItem(String question, float latitude, float longitude){
+        this.question = question;
+        targetLocation = new LatLng(latitude, longitude);
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public LatLng getTargetLocation(){
+        return targetLocation;
+    }
+
 }
