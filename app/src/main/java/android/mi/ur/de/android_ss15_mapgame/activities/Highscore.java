864b6f2bc947b27544d1896d552d6b2dc1ff048c
raffaela007 <raffaela.schierle@stud.uni-regensburg.com>
2015-09-13 20:46:02 +0200

    #14 UI: Creation of layout for online highscore

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Highscore.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Highscore.java
@@ -1,9 +1,34 @@
 package android.mi.ur.de.android_ss15_mapgame.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MainActivity;
+import android.mi.ur.de.android_ss15_mapgame.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Daniel on 24.08.2015.
  */
 public class Highscore extends Activity{
+
+    private Button menu;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore);
+
+
+        menu = (Button) findViewById(R.id.highscoreMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(Highscore.this, MainActivity.class);
+                startActivity(nextActivity);
+            }
+        });
+
+    }
+
 }
