1cdf0007a350a1873077fd7011917d94d6139881
raffaela007 <raffaela.schierle@stud.uni-regensburg.com>
2015-09-13 20:47:44 +0200

    #18 Implementation of buttons for all activities

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameResult.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameResult.java
@@ -1,8 +1,12 @@
 package android.mi.ur.de.android_ss15_mapgame.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MainActivity;
 import android.mi.ur.de.android_ss15_mapgame.R;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -12,6 +16,9 @@ public class GameResult extends Activity {
     private String score;
     private TextView highscoreValue;
 
+    private Button again;
+    private Button menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +27,30 @@ public class GameResult extends Activity {
         score = extras.getString("score");
 
         setupUI();
+
+        again = (Button) findViewById(R.id.playagain);
+        again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(GameResult.this, GameStart.class);
+                startActivity(nextActivity);
+            }
+        });
+
+        menu = (Button) findViewById(R.id.resultMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(GameResult.this, MainActivity.class);
+                startActivity(nextActivity);
+            }
+        });
     }
 
     private void setupUI(){
         setContentView(R.layout.game_result);
 
-        highscoreValue = (TextView) findViewById(R.id.highscoreValue);
+        highscoreValue = (TextView) findViewById(R.id.scoreValue);
         highscoreValue.setText(score);
 
     }
--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameStart.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/GameStart.java
@@ -1,9 +1,87 @@
 package android.mi.ur.de.android_ss15_mapgame.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MainActivity;
+import android.mi.ur.de.android_ss15_mapgame.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Daniel on 24.08.2015.
  */
 public class GameStart extends Activity {
+
+    private static final int GERMANY = 0;
+    private static final int EUROPE = 1;
+    private static final int WORLD = 2;
+
+    private Button germany;
+    private Button europa;
+    private Button worldwide;
+    private Button menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setupUI();
+
+
+        germany = (Button) findViewById(R.id.germany);
+        germany.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(GameStart.this, GameActivity.class);
+                nextActivity.putExtra("region", GERMANY);
+                startActivity(nextActivity);
+            }
+        });
+
+
+        europa = (Button) findViewById(R.id.europa);
+        europa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent nextActivity = new Intent(GameStart.this, GameActivity.class);
+                nextActivity.putExtra("region", EUROPE);
+                startActivity(nextActivity);
+
+            }
+        });
+
+
+        worldwide = (Button) findViewById(R.id.worldwide);
+        worldwide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent nextActivity = new Intent(GameStart.this, GameActivity.class);
+                nextActivity.putExtra("region", WORLD);
+                startActivity(nextActivity);
+
+            }
+        });
+
+
+        menu = (Button) findViewById(R.id.gamestartMenu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(GameStart.this, MainActivity.class);
+                startActivity(nextActivity);
+            }
+        });
+
+
+    }
+
+    private void setupUI(){
+        setContentView(R.layout.game_start);
+
+
+    }
 }
