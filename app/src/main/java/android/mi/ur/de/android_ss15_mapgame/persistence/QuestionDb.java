30b07743376273d7071db294288242ff2d61ca84
Daniel192 <d.hecht13@gmx.de>
2015-09-13 13:58:15 +0200

    Added: #16 Regions
    Added regions to all questions in the db.

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/persistence/QuestionDb.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/persistence/QuestionDb.java
@@ -23,11 +23,17 @@ public class QuestionDb {
     private static final String KEY_QUESTION = "question";
     private static final String KEY_LATITUDE = "latitude";
     private static final String KEY_LONGITUDE = "longitude";
+    private static final String KEY_REGION = "region";
 
     private static final int COLUMN_ID_INDEX = 0;
     private static final int COLUMN_QUESTION_INDEX = 1;
     private static final int COLUMN_LATITUDE_INDEX = 2;
     private static final int COLUMN_LONGITUDE_INDEX = 3;
+    private static final int COLUMN_REGION_INDEX = 4;
+
+    private static final int GERMANY = 0;
+    private static final int EUROPE = 1;
+    private static final int WORLD = 2;
 
 
     private QuestionDbOpenHelper dbHelper;
@@ -69,6 +75,7 @@ public class QuestionDb {
         return null;
     }
 
+    //Returns all QuestionItems in the database
     public ArrayList<QuestionItem> getAllQuestionItems() {
         ArrayList<QuestionItem> items = new ArrayList<QuestionItem>();
         Cursor cursor = db.query(DATABASE_TABLE, new String[]{KEY_ID, KEY_QUESTION, KEY_LATITUDE, KEY_LONGITUDE}, null, null, null, null, null);
@@ -85,9 +92,28 @@ public class QuestionDb {
         return items;
     }
 
+    //Returns all QuestionItems for the specified region (germany = 0, europe = 1, world = 2)
+    public ArrayList<QuestionItem> getAllQuestionItems(int region) {
+        ArrayList<QuestionItem> items = new ArrayList<QuestionItem>();
+        Cursor cursor = db.query(DATABASE_TABLE, new String[]{KEY_ID, KEY_QUESTION, KEY_LATITUDE, KEY_LONGITUDE}, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                if (cursor.getInt(COLUMN_REGION_INDEX) <= region) {
+                    String question = cursor.getString(COLUMN_QUESTION_INDEX);
+                    float latitude = cursor.getFloat(COLUMN_LATITUDE_INDEX);
+                    float longitude = cursor.getFloat(COLUMN_LONGITUDE_INDEX);
+
+                    items.add(new QuestionItem(question, latitude, longitude));
+                }
+
+            } while (cursor.moveToNext());
+        }
+        return items;
+    }
+
 
     private class QuestionDbOpenHelper extends SQLiteOpenHelper {
-        private final String DATABASE_CREATE = "create table " + DATABASE_TABLE + " (" + KEY_ID + " integer primary key autoincrement not null, " + KEY_QUESTION + " varchar(255), " + KEY_LATITUDE + " float, " + KEY_LONGITUDE + " float);";
+        private final String DATABASE_CREATE = "create table " + DATABASE_TABLE + " (" + KEY_ID + " integer primary key autoincrement not null, " + KEY_QUESTION +  "varchar(255), " + KEY_LATITUDE + " float, " + KEY_LONGITUDE + " float, " + KEY_REGION + " integer);";
 
         public QuestionDbOpenHelper(Context c, String dbname, SQLiteDatabase.CursorFactory factory, int version) {
             super(c, dbname, factory, version);
@@ -96,11 +122,11 @@ public class QuestionDb {
         @Override
         public void onCreate(SQLiteDatabase db) {
             db.execSQL(DATABASE_CREATE);
-            db.execSQL(addQuestion("Wo liegt Altötting?", 48.2263996f, 12.6701338f));
-            db.execSQL(addQuestion("Wo befindet sich Coburg?", 50.2603389f, 10.9755166f));
-            db.execSQL(addQuestion("Wo ist Erfurt?", 50.9853404f, 11.0153355f));
-            db.execSQL(addQuestion("Wo befindet sich Frankfurt am Main", 50.121212f, 8.6365638f));
-            db.execSQL(addQuestion("Wo ist Regensburg?", 48.9940947f, 12.0750918f));
+            db.execSQL(addQuestion("Wo liegt Altötting?", 48.2263996f, 12.6701338f, GERMANY));
+            db.execSQL(addQuestion("Wo befindet sich Coburg?", 50.2603389f, 10.9755166f, GERMANY));
+            db.execSQL(addQuestion("Wo ist Erfurt?", 50.9853404f, 11.0153355f, GERMANY));
+            db.execSQL(addQuestion("Wo befindet sich Frankfurt am Main", 50.121212f, 8.6365638f, GERMANY));
+            db.execSQL(addQuestion("Wo ist Regensburg?", 48.9940947f, 12.0750918f, GERMANY));
         }
 
         @Override
@@ -108,8 +134,8 @@ public class QuestionDb {
 
         }
 
-        private String addQuestion(String question, float latitude, float longitude) {
-            String insertClause = "insert into " + DATABASE_TABLE + " (" + KEY_QUESTION + ", " + KEY_LATITUDE + ", " + KEY_LONGITUDE + ") values ('" + question + "', " + latitude + ", " + longitude + ")";
+        private String addQuestion(String question, float latitude, float longitude, int region) {
+            String insertClause = "insert into " + DATABASE_TABLE + " (" + KEY_QUESTION + ", " + KEY_LATITUDE + ", " + KEY_LONGITUDE + ", " + KEY_REGION + ") values ('" + question + "', " + latitude + ", " + longitude + ", " + region + ")";
             return insertClause;
         }
 
