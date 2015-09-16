4a0b391721fbeb03d837ac3ec807aa12d8bced3c
raffaela007 <raffaela.schierle@stud.uni-regensburg.com>
2015-09-16 19:18:48 +0200

    #19 Creation of info-screen

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Info.java
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Info.java
@@ -1,9 +1,35 @@
 package android.mi.ur.de.android_ss15_mapgame.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MainActivity;
+import android.mi.ur.de.android_ss15_mapgame.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Daniel on 24.08.2015.
  */
-public class Info extends Activity{
+public class Info extends Activity {
+
+    Button infoMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.info);
+
+        infoMenu = (Button) findViewById(R.id.infoMenu);
+        infoMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(Info.this, MainActivity.class);
+                startActivity(nextActivity);
+            }
+        });
+
+    }
+
 }
