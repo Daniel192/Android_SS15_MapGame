bec85d9377a12a4424a125f4ffe1e5b5d56f8e20
raffaela007 <raffaela.schierle@stud.uni-regensburg.com>
2015-09-25 16:39:46 +0200

    #34 Add sources

--- app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Settings.java
+++
@@ -1,9 +0,0 @@
-package android.mi.ur.de.android_ss15_mapgame.activities;
-
-import android.app.Activity;
-
-/**
- * Created by Daniel on 24.08.2015.
- */
-public class Settings extends Activity {
-}
---
+++ app/src/main/java/android/mi/ur/de/android_ss15_mapgame/activities/Sources.java
@@ -0,0 +1,35 @@
+package android.mi.ur.de.android_ss15_mapgame.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.mi.ur.de.android_ss15_mapgame.MainActivity;
+import android.mi.ur.de.android_ss15_mapgame.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Daniel on 24.08.2015.
+ */
+public class Sources extends Activity {
+
+    Button sourcesMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.info);
+
+        sourcesMenu = (Button) findViewById(R.id.infoMenu);
+        sourcesMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(Sources.this, MainActivity.class);
+                startActivity(nextActivity);
+            }
+        });
+
+    }
+
+}
